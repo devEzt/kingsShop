f875e5e93a703fb235537fbc98bbce548097aed5
Alexandre Maciel <eztalenda@gmail.com>
2021-04-03 09:45:08 -0300

    bug fix

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography} from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form'
 
@@ -6,14 +6,26 @@ import { commerce } from '../../lib/commerce';
 
 import FormInput from './CustomTextField';
 
-const AddressForm = () => {
+const AddressForm = ({ checkoutToken }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
     const [shippingSubdivision, setShippingSubdivision] = useState('');
     const [shippingOptions, setShippingOptions] = useState([]);
-
+    const [shippingOption, setShippingOption] = useState('');
     const methods = useForm();
+
+    const fetchShippingCountries = async(checkoutTokenId) => {
+        const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+        console.log(countries);
+        setShippingCountries(countries);
+        setShippingCountry(countries.AT);
+    }
+
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id)
+    }, []);
 
     return (
         <>
@@ -27,7 +39,7 @@ const AddressForm = () => {
                         <FormInput required name='email' label='Email'/>
                         <FormInput required name='city' label='City'/>
                         <FormInput required name='zip' label='ZIP / Postal Code'/>
-                        <Grid item xs={12} sm={6}>
+                        {/* <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
                             <Select value={} fullWidth onChange={}>
                                 <MenuItem key={} value={}>
@@ -50,7 +62,7 @@ const AddressForm = () => {
                                     Select Me
                                 </MenuItem>
                             </Select>
-                        </Grid>
+                        </Grid> */}
                     </Grid>
                 </form>
             </FormProvider>
